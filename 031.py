a22e1e2e7858ff11c45672a1ae92556911e69a36
HengZh <hzhang67@usc.edu>
2020-08-05 00:09:51 -0500

    V2

--- 031.py
+++ 031.py
@@ -4,18 +4,23 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         N = len(nums)
+        
         i = N-1
-        for i in range(N-1, -1, -1):
-            if i == 0:
-                nums.sort()
-                return
-            if nums[i] > nums[i-1]:
+        while i > 0:
+            if nums[i-1] < nums[i]:
                 break
-        nums[i:] = sorted(nums[i:])
-        for j in range(i, N):
-            if nums[j] > nums[i-1]:
-                tmp = nums[j]
-                nums[j] = nums[i-1]
-                nums[i-1] = tmp
-                return
+            i -= 1
+
+        if i > 0:
+            j = N - 1
+            while j >= i:
+                if nums[j] > nums[i-1]:
+                    nums[i-1], nums[j] = nums[j], nums[i-1]
+                    break
+                j -= 1
+        
+        j = N - 1
+        while j > i:
+            nums[i], nums[j] = nums[j], nums[i]
+            j -= 1; i += 1
         
